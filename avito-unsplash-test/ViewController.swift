f20ac599754f360cb9e0752e85040db232d96e9f
Mark Kulik <kulik_mark@mail.ru>
2024-09-07 21:06:20 +0300

    Initial Commit

---
+++ avito-unsplash-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  avito-unsplash-test
+//
+//  Created by Марк Кулик on 07.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
